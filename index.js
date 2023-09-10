853241f94482351054d3bfb7b8cda8a2abf2108b
Burak Ülkü <brkulku@gmail.com>
2023-09-10 19:01:07 +0200

    Generate color

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-console.log('hello');
-var randomHex = require('random-hex');
+// Import the necessary libraries
+import chalk from 'chalk';
 
-randomHex.generate(); // #f92672
-randomHex.generate(); // #f060ff
+// Function to generate a random hex color code
+function getRandomHexColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-console.log(random - hex);
+// Function to generate and display the colored block
+function generateColoredBlock() {
+  const color = getRandomHexColor();
+  const block = '#'.repeat(31) + '\n';
+  const coloredBlock = chalk.hex(color)(block);
+  console.log(coloredBlock);
+}
+
+// Main function to run the application
+function main() {
+  generateColoredBlock();
+}
+
+// Run the main function
+main();
